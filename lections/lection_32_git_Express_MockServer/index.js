89701170c0c558ad76d9dacb356e222c6edd7765
Savelii Sychov <savelii@miro.com>
2022-07-04 19:36:06 +0200

    m5

--- lections/lection_32_git_Express_MockServer/index.js
+++ lections/lection_32_git_Express_MockServer/index.js
@@ -2,3 +2,15 @@ const x = 30;
 
 console.log(1);
 console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
+console.log(525, 925);
